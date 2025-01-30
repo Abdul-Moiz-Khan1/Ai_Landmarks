3a979c1db7e30be62278a6d405223649c4391ffd
Abdul-Moiz-Khan1 <moizz1326@gmail.com>
2025-01-30 15:35:30 +0500

    first commit

---
+++ app/src/main/java/com/example/jetpack_ai_landmark/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.jetpack_ai_landmark
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpack_ai_landmark.ui.theme.Jetpack_Ai_landmarkTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Jetpack_Ai_landmarkTheme {
+              
+            }
+        }
+    }
+}
+
