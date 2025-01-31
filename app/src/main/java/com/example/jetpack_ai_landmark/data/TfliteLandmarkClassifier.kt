a302c10ea1694e4618b1bc139434712c684dbf93
Abdul-Moiz-Khan1 <moizz1326@gmail.com>
2025-01-31 17:59:58 +0500

    first

--- app/src/main/java/com/example/jetpack_ai_landmark/data/TfliteLandmarkClassifier.kt
+++ app/src/main/java/com/example/jetpack_ai_landmark/data/TfliteLandmarkClassifier.kt
@@ -2,9 +2,13 @@ package com.example.jetpack_ai_landmark.data
 
 import android.content.Context
 import android.graphics.Bitmap
+import androidx.camera.core.ImageProcessor
+import androidx.compose.material3.Surface
 import com.example.jetpack_ai_landmark.domain.Classification
 import com.example.jetpack_ai_landmark.domain.LandmarkClassifier
+import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.core.vision.ImageProcessingOptions
 import org.tensorflow.lite.task.vision.classifier.ImageClassifier
 
 class TfliteLandmarkClassifier(
@@ -37,7 +41,23 @@ class TfliteLandmarkClassifier(
 
 
     override fun classify(bitmap: Bitmap, rotation: Int): List<Classification> {
+        if (classifier == null) {
+            setUpClassifier()
+        }
+        val imageProcessor = org.tensorflow.lite.support.image.ImageProcessor.Builder().build()
+        val tensorImage = imageProcessor.process(TensorImage.fromBitmap(bitmap))
+        val imageProcessingOptions
 
+    }
+
+    private fun getOrientationFromRotation(rotation: Int): ImageProcessingOptions.Orientation {
+        return when (rotation) {
+            android.view.Surface.ROTATION_0 -> ImageProcessingOptions.Orientation.RIGHT_TOP
+            android.view.Surface.ROTATION_90 -> ImageProcessingOptions.Orientation.TOP_LEFT
+            android.view.Surface.ROTATION_180 -> ImageProcessingOptions.Orientation.RIGHT_BOTTOM
+            else -> ImageProcessingOptions.Orientation.BOTTOM_RIGHT
+
+        }
     }
 
 
